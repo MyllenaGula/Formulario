714a8337d0ee11ce9537434b90a412f3d3286be7
Myllena Tratsch Gula <gula.myllena@escola.pr.gov.br>
2025-09-08 12:02:19 -0300

    4ht

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave"); 
 
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
 
 function mostraPalavrasChave() { 
   alert("fui clicado!"); 
-} 
+}
+
