6de815ba996e3ce2f3b001392ca8bb9a1dffe890
Myllena Tratsch Gula <gula.myllena@escola.pr.gov.br>
2025-09-15 11:51:54 -0300

    giyeg

--- script.js
+++ script.js
@@ -29,7 +29,15 @@ function ordenaPalavra(p1, p2) {
     return ordenadas.slice(0, 10);
 }
 
-
+function tiraPalavrasRuins(palavras) {
+    const palavrasBoas = [];
+    for (let palavra of palavras) {
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+            palavrasBoas.push(palavra);
+        }
+    }
+    return palavrasBoas;
+}
 
 
 
