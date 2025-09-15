3fb46ca13bab8728ec3f9c698c38ad1772a64a6f
Myllena Tratsch Gula <gula.myllena@escola.pr.gov.br>
2025-09-15 11:19:22 -0300

    js

--- script.js
+++ script.js
@@ -4,7 +4,16 @@ const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
 
-function mostraPalavrasChave() { 
-  alert("fui clicado!"); 
-}
+function mostraPalavrasChave(){
+let palavras = texto.split(/\P{L}+/u);
+
+    for (let i in palavras) {
+        palavras[i] = palavras[i].toLowerCase();
+    }
+
+    palavras = tiraPalavrasRuins(palavras);
+
+    const frequencias = contaFrequencias(palavras)
+} 
+
 
