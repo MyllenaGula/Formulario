eeb6e2cfbd02cec592a5220be109424568aca84a
Myllena Tratsch Gula <gula.myllena@escola.pr.gov.br>
2025-09-15 11:24:01 -0300

    js

--- script.js
+++ script.js
@@ -14,6 +14,14 @@ let palavras = texto.split(/\P{L}+/u);
     palavras = tiraPalavrasRuins(palavras);
 
     const frequencias = contaFrequencias(palavras)
+
+    function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
+
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+    return ordenadas.slice(0, 10);
+}
 } 
 
 
