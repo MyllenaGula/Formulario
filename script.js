1801e78613437231f12d9292e74fd51d2f6e0b6d
Myllena Tratsch Gula <gula.myllena@escola.pr.gov.br>
2025-09-15 11:41:20 -0300

    u

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import {PALAVRAS_RUINS } from "./palavrasRuins.js";
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave"); 
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
+
 function mostraPalavrasChave() {
     const texto = document.querySelector("#entrada-de-texto").value;
     const campoResultado = document.querySelector("#resultado-palavrachave");
@@ -19,6 +20,14 @@ function processaTexto(texto) {
     palavras = tiraPalavrasRuins(palavras);
 
     const frequencias = contaFrequencias(palavras);
+
+    function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
+
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+    return ordenadas.slice(0, 10);
+}
 
 function mostraPalavrasChave(){
 let palavras = texto.split(/\P{L}+/u);
@@ -39,6 +48,7 @@ function ordenaPalavra(p1, p2) {
     let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
     return ordenadas.slice(0, 10);
 }
+
 
 
 
